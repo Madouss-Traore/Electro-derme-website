d5a021bcc81b09f5472a0a571cb996c62f1afab6
Madouss-Traore <madouss.traore.dev@gmail.com>
2025-08-28 17:57:47 +0200

    Setup routes and render for main pages, configure views path

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 import "dotenv/config";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT;
 
 
 app.set("view engine", "ejs");
-app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use(express.static('./public'));
 
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.render('pages/home');
 });
+
+app.get('/electrolyse', (req, res) => {
+    res.render('pages/electrolysis');
+});
+
+app.get('/tarifs', (req, res) => {
+    res.render('pages/pricing');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('pages/contact');
+});
+
+app.get('/faq', (req, res) => {
+    res.render('pages/faq');
+});
+
+
 
 
 // PORT 
